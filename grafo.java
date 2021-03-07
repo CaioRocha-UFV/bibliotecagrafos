f0c5db32ff921be2a0ee903c674587bf206d7f33
Erian <70418490+Erian-A@users.noreply.github.com>
2021-03-07 17:50:06 -0300

    tá funcionando

--- grafo.java
+++ grafo.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+
+
+//import java.util.Scanner;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -10,12 +12,26 @@ class Grafo{
     int numeroDeVertices;
     ArrayList<Vertice> grafo;
 
+    void CriarGrafo(String fileName) throws FileNotFoundException, IOException{
 
-    private void CriarGrafo(String fileName){
-
-
+        grafo = new ArrayList<Vertice>();
+        grafo = LeituraDeArquivo(fileName);
+        
     }
-
+    
+    // Função protótipo para exibir os vértices no grafo e seus vizinhos
+    void ExibirGrafo(){
+        
+        for (Vertice vertice : grafo){
+            System.out.print("Vertice: " + vertice.index);
+            System.out.print(" Vizinhos: ");
+            
+            for (Aresta aresta : vertice.vizinhos){
+                System.out.print(aresta.verticeAlvo.index + " ");
+            }
+            System.out.println("");
+        }
+    }
     // Le um arquivo .txt e retorna uma matriz (?) ou uma lista de adjacencia
     private ArrayList<Vertice> LeituraDeArquivo (String fileName)  throws FileNotFoundException, IOException{
         BufferedReader reader = null;
@@ -26,8 +42,8 @@ class Grafo{
             int numDeVertices = 0;
 
             // Setup da leitura do Arquivo
-            pathName = System.getProperty("user.home") + "\\Desktop\\";
-            reader = new BufferedReader(new FileReader(pathName + fileName));
+            //pathName = System.getProperty("user.home") + "\\Desktop\\";
+            reader = new BufferedReader(new FileReader( fileName));
             
             // Numero de vértices a partir da primeira linha
             numDeVertices = Integer.parseInt(reader.readLine());
@@ -46,27 +62,33 @@ class Grafo{
                 float peso = Float.parseFloat(tokens[2]);
                 Vertice vertice1;
                 Vertice vertice2;
+                int[] retornoFuncao;
 
-
+                // Pega o retorno da verificação se existe um vértice com aquele índice
+                retornoFuncao = ExisteVerticeComIndex(adjList, index1);
                 // Testa se os vértices ja estão na lista
-                if (ExisteVerticeComIndex(index1) == false){
-                    // Se nao estiver cria
+                // e adiciona eles caso não estejam
+                if (retornoFuncao[0] == 0){
                     vertice1 = new Vertice(index1);
-                } else {
-                    // Se estive, get
-                    vertice1 = adjList.get(index1);
-                }
-                if (ExisteVerticeComIndex(index2) == false){
-                    vertice2 = new Vertice(index2);
+                    adjList.add(vertice1);    
                 } else{
-                    vertice2 = adjList.get(index2);
+                    vertice1 = adjList.get(retornoFuncao[1]);
                 }
-
+                
+                retornoFuncao = ExisteVerticeComIndex(adjList, index2);
+                if (retornoFuncao[0] == 0){
+                    vertice2 = new Vertice(index2);
+                    adjList.add(vertice2); 
+                } else{
+                    vertice2 = adjList.get(retornoFuncao[1]);
+                }
+           
                 // Testa se aresta já existe
                 if (ExisteArestaEntre(vertice1, vertice2) == false){
                     CriaAresta(vertice1, vertice2, peso);
                 }
             }
+            return adjList;
             
         } finally{
             try{
@@ -78,25 +100,41 @@ class Grafo{
         }
     }
 
+    // Verifica se existe vértice com determinado índice
+    // Retorna vetor informando a existência e a posição onde está o vértice com aquele índice
     // TODO
-    public boolean ExisteVerticeComIndex (int index){
-
-        return true;
+    public int[] ExisteVerticeComIndex ( ArrayList<Vertice> adjList, int index){
+        // Caso não exista o vértice retorna 0 e posição -1
+        // Caso exista retorna 1 e a posição do objeto na lista
+        int[] ExistenciaEPosicao = {0, -1};
+        for (Vertice vert1 : adjList){
+            if (vert1.index == index){
+                ExistenciaEPosicao[0] = 1;
+                ExistenciaEPosicao[1] = adjList.indexOf(vert1);
+                return ExistenciaEPosicao;
+            }
+        }
+        return ExistenciaEPosicao;
     }
 
     // TODO
     public boolean ExisteArestaEntre(Vertice vertice1, Vertice vertice2){
-
-        return true;
+        
+        
+        for (Aresta aresta : vertice1.vizinhos){
+            
+            if (aresta.verticeAlvo.equals(vertice2)){
+                return true;
+            }
+        }
+        return false;
     }
 
     // Cria uma aresta a partir de dois vértices e um peso
-    Vertice[] CriaAresta(Vertice vertice1, Vertice vertice2, float peso){
+    void CriaAresta(Vertice vertice1, Vertice vertice2, float peso){
+        
         vertice1.vizinhos.add(new Aresta(peso, vertice2));
-        vertice2.vizinhos.add(new Aresta(peso, vertice1));
-
-        Vertice[] vertices = {vertice1, vertice2};
-        return vertices; // CHECAR SE PRECISA RETORNAR (TALVEZ JA TENHA MUDADO O VALOR REAL)
+        vertice2.vizinhos.add(new Aresta(peso, vertice1));    
     }
 
     // 
@@ -108,7 +146,7 @@ class Grafo{
         public Vertice (int index){
             this.index = index;
             marcado = false;
-            ArrayList<Aresta> vizinhos = new ArrayList<Aresta>();
+            vizinhos = new ArrayList<Aresta>();
         }
 
         public int NumeroDeVizinhos(){
--- main.java
+++ main.java
@@ -1,5 +1,22 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class main {
-    public static void main(String[] args){
-        System.out.println("Hello there!");
+   
+    public static void main(String[] args) throws IOException {
+    
+        String arquivo;
+        Grafo shy = new Grafo();
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Nome do arquivo: ");
+        arquivo = entrada.next();
+        
+        // Passa o nome do arquivo e cria o grafo daqueles dados
+        shy.CriarGrafo(arquivo);
+        
+        // Mostra o grafo, seus vértices e quem são seus vizinhos
+        shy.ExibirGrafo();
     }
 }
